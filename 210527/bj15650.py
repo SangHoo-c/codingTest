448dfd0d33ba3cd1169162df811f651b0013c079
SangHoo-c <59246354+SangHoo-c@users.noreply.github.com>
2021-05-27 16:28:36 +0900

    중복 체크 삭제

--- 210527/bj15650.py
+++ 210527/bj15650.py
@@ -1,13 +1,9 @@
+# 오름차순의 경우, 중복체크를 할 필요가 없다. 
+# 비교만 수행하면 된다. 
+
 n, m = map(int, input().split())
 
-vis = [0] * (n+1)       # 방문을 체크하는 배열
 arr = [0] * (m+1)       # 가능한 경우의 수를 담을 배열 
-
-def promising(idx, num):  # 가능한 경우를 체크하는 함수 
-    if vis[num] == 0 and arr[idx-1] < num:
-        return True
-    else:
-        return False
 
 def dfs(x):
     if x == m+1:
@@ -16,10 +12,8 @@ def dfs(x):
         print()
     else:
         for i in range(1, n+1):     # 모든 경우(자식 노드)에 대해 
-            if promising(x, i):     # 가능여부를 체크 
-                vis[i] = 1
+            if arr[x-1] < i:        # 가능여부를 체크, promising() 역할을 한다.    
                 arr[x] = i
                 dfs(x+1)
-                vis[i] = 0
                 arr[x] = 0
 dfs(1)
