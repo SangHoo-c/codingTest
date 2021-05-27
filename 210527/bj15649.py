547d4743c2697644e444a80a4bec559904f29dce
SangHoo-c <59246354+SangHoo-c@users.noreply.github.com>
2021-05-27 15:56:56 +0900

    주석추가

--- 210527/bj15649.py
+++ 210527/bj15649.py
@@ -1,8 +1,9 @@
 n, m = map(int, input().split())
 
-vis = [0] * (n+1)
-arr = [0] * (m+1)
+vis = [0] * (n+1)       # 방문 여부 확인 
+arr = [0] * (m+1)       # 가지별 가능한 수열 
 
+# 모든 자식 노드를 확인할때, 가능한지 확인하는 함수 
 def promising(idx):
     if vis[idx] == 0:
         return True
@@ -15,12 +16,12 @@ def dfs(x):
             print(arr[i], end=' ')
         print()
     else:
-        for i in range(1,n+1):
-            if promising(i):
+        for i in range(1,n+1):      # 가능한 모든 노드 확인 
+            if promising(i):        # promising 함수를 통해 가능여부 체크 
                 vis[i] = 1
                 arr[x] = i
-                dfs(x+1)
-                vis[i] = 0
-                arr[x] = 0
+                dfs(x+1)            # 자식 노드로 내려간다. 
+                vis[i] = 0          # 다시 부모노드로 올라간다. 
+                arr[x] = 0          
 
-dfs(1)
+dfs(1)         
