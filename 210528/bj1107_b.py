6d7042df9542bfc3bc0b53848565d90baea776d1
SangHoo-c <59246354+SangHoo-c@users.noreply.github.com>
2021-05-31 15:35:37 +0900

    예외 처리 추가

--- 210528/bj1107_b.py
+++ 210528/bj1107_b.py
@@ -4,23 +4,29 @@
 # for #2 len <= 6
 # O(N * len)
 
+# 예외 1. 모든 숫자가 고장난 경우 
+# 예외 2. 고장난 숫자가 없는 경우 
+
 N = int(input())
 M = int(input())
 
-broken_btn = list(map(int, input().split()))
 min_value = 1e9
+enable = {str(i) for i in range(10)}
 
-for i in range(1000000):
+
+if M != 0:
+    enable -= set(map(str, input().split()))  # 고장난 버튼 리스트 제거
+
+    
+for i in range(1000001):
     cnt = 0
     input_str = str(i)
     input_len = len(input_str)
 
     for j in range(input_len):
-        if int(input_str[j]) not in broken_btn:
-            cnt += 1
-
-    if cnt == input_len:
-        min_value = min(min_value, abs(N - i) + input_len)
-
+        if input_str[j] not in enable:
+            break
+        elif j == input_len - 1:
+            min_value = min(min_value, abs(N - i) + input_len)
 
 print(min(min_value, abs(N - 100)))
