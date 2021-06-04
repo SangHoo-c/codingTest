8846ac03ce0f030acdebd40d5f47c0d93669c7dd
SangHoo-c <59246354+SangHoo-c@users.noreply.github.com>
2021-06-05 00:00:49 +0900

    Update bj2437.py

--- 210604/bj2437.py
+++ 210604/bj2437.py
@@ -1,16 +1,17 @@
+# 브루스 포스, 순열, 조합 모두 시간초과가 난다. 
+# 점화식을 찾는다. 
+# dp (1~n) 까지의 규칙, greedy (n-1 ~ n) 규칙 
+
 import sys
 
 N = int(sys.stdin.readline().strip())
 weight = list(map(int, sys.stdin.readline().split(" ")))
 max = 1
 weight.sort()
-# sorted(weight, key= lambda x : x)
-# print(weight)
 
 for i in range(N):
     if max >= weight[i]:
         max += weight[i]
     else:
-        # max += 1
         break
 print(max)
