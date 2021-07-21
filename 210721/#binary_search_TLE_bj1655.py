81854e61886b418133375aa2f60e964089b1cc00
SangHoo-c <59246354+SangHoo-c@users.noreply.github.com>
2021-07-22 00:38:06 +0900

    fix(binary-search) : TLE solved

--- 210721/#binary_search_TLE_bj1655.py
+++ 210721/#binary_search_TLE_bj1655.py
@@ -40,3 +40,40 @@ def bin_search(l, r, x):
 
 
 main()
+
+
+
+--------------------------------------
+"""
+TLE solved 
+"""
+
+
+from collections import deque
+import sys
+
+a = int(sys.stdin.readline())
+result = deque()
+real = deque()
+for i in range(a):
+    b = int(sys.stdin.readline())
+    x = 0
+    y = len(result) - 1
+    while x <= y:
+        mid = (x + y) // 2
+        if result[mid] < b:
+            x = mid + 1
+        else:
+            y = mid - 1
+    if x >= len(result):
+        result.append(b)
+    else:
+        result.insert(x, b)
+
+    if len(result) % 2 == 0:
+        real.append(result[((len(result) + 1) // 2) - 1])
+    else:
+        real.append(result[((len(result) + 1) // 2) - 1])
+for i in real:
+    print(i)
+
