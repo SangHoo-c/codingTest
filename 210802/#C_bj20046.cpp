9373c43580cf1101e08e30dd24a7ee3824ceea17
LEE SANG HOO <59246354+SangHoo-c@users.noreply.github.com>
2021-08-02 14:30:57 +0900

    algo(C ++ ) : refactoring, pq-overloading

--- 210802/#C_bj20046.cpp
+++ 210802/#C_bj20046.cpp
@@ -4,15 +4,16 @@
 
 using namespace std;
 
-typedef struct {
+struct Node{
     int cost;
     int x;
     int y;
-}Node;
+};
 
 struct cmp{
     bool operator()(Node n1, Node n2){
         return n1.cost > n2.cost;
+        // 최소힙
     }
 };
 
@@ -51,6 +52,9 @@ int main(void){
     }
 
     Node node = {arr[0][0], 0, 0};
+    
+    // priority_queue 를 사용하는 방법
+    // priority queue 에 overload 하는 과정 
     priority_queue<Node, vector<Node>, cmp> heap;
     heap.push(node);
     visited[0][0] = arr[0][0];
@@ -61,21 +65,19 @@ int main(void){
         x = heap.top().x;
         y = heap.top().y;
         heap.pop();
-//        printf("%d %d \n", x, y);
 
         for(int i = 0; i < 4 ; i ++){
-            int r, c;
+            int r, c, nxt_cost;
             r = x + dx[i];
             c = y + dy[i];
 
             if(0 <= r  && r < m && 0 <= c && c < n){
                 if(arr[r][c] != -1){
                     if(visited[r][c] == -1 || visited[r][c] > cost + arr[x][y]){
-                        int nxt_cost;
                         nxt_cost = cost + arr[r][c];
-                        Node tmp_node = {nxt_cost, r, c};
                         visited[r][c] = nxt_cost;
-                        heap.push(tmp_node);
+                        // Node tmp_node = {nxt_cost, r, c}; 이와 같이 선언하지 않고, 직접 {} 을 이용해 넣어준다.
+                        heap.push({nxt_cost, r, c});
                     }
                 }
             }
