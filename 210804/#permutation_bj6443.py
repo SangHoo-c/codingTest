2419177d040813b2ff2e2f5e1cd61783bfdc652d
LEE SANG HOO <59246354+SangHoo-c@users.noreply.github.com>
2021-08-04 20:57:36 +0900

    algo(permutation) : add comments

--- 210804/#permutation_bj6443.py
+++ 210804/#permutation_bj6443.py
@@ -18,6 +18,8 @@ for _ in range(N):
             return
 
         for i in range(s_len):
+            # 연속되는 숫자는 가장 처음 나오는 수만 root 로 인정해준다.
+            # 즉, 두번째 수가 나오려면, 무조건 바로 이전의 수가 이미 뽑혀있어야한다. 
             if visited[i] == 0 and (i == 0 or s[i - 1] != s[i] or visited[i - 1]):
                 arr.append(s[i])
                 visited[i] = 1
