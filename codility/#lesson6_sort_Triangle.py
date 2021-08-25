ad99e304f6d2d39cb46b8621e8da52c9c676e5e5
LEE SANG HOO <59246354+SangHoo-c@users.noreply.github.com>
2021-08-25 18:01:54 +0900

    algo : time-complexity

---
+++ codility/#lesson6_sort_Triangle.py
@@ -0,0 +1,21 @@
+"""
+  효율성 문제
+  1. A 를 sort 한다.
+  2. 원소간의 차를 구한다. 
+  
+  => O(N) 으로 처리 가능
+"""
+
+def solution(A):
+    A.sort()
+    B = [0] * (len(A) - 1)
+    for i in range(len(A)-1):
+        B[i] = A[i+1] - A[i]
+    
+    res = 0
+    for i in range(1, len(B)):
+        if A[i - 1] > B[i]:
+            res = 1
+            break
+    
+    return res
